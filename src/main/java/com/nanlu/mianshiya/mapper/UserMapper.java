d4128765bf26c5e09945312b6b4bccb7c9be4700
indexhtmlhtml <1300390684@qq.com>
2024-12-26 18:54:09 +0800

    Changes

---
+++ src/main/java/com/nanlu/mianshiya/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.nanlu.mianshiya.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.nanlu.mianshiya.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
